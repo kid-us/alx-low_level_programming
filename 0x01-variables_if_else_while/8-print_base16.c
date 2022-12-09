5a3dd831a874f6846c7cdc23633dccfe26f2cc08
kid-us <kidushh29@gmail.com>
2022-12-09 00:41:26 -0800

    Hexadecimal Numbers from a-f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char h;
+
+	int n;
+
+	h = 'a';
+	n = 0;
+	while
+		(n < 10) {
+			putchar(n + '0');
+			n++;
+		}
+	while
+		(h <= 'f') {
+			putchar(h);
+			h++;
+		}
+	putchar('\n');
+	return (0);
+}
