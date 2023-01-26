8716ee9968b232bd8b6f5441389dcb84c3013ae7
kid-us <kidushh29@gmail.com>
2023-01-26 12:05:54 -0800

    Free List

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,14 +16,15 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	
+
 	if (new == NULL)
 		return (NULL);
 
 	dup = strdup(str);
+
 	if (dup == NULL)
 	{
-		free(new);
+		free(dup);
 		return (NULL);
 	}
 
