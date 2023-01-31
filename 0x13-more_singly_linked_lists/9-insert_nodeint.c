4779dde8b4c636bf030df9bef4bdef0dd4000054
kid-us <kidushh29@gmail.com>
2023-01-31 13:16:57 -0800

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node to a listint_t list at a given position.
+ * insert_nodeint_at_index - Inserts a new node to
+ * a listint_t list at a given position.
  * @head: A pointer to the address of the head of the listint_t list.
- * @idx: The index of the listint_t list where the new node should be added - indices start at 0.
+ * @idx: The index of the listint_t list where the
+ * new node should be added - indices start at 0.
  * @n: The integer for the new node to contain.
  * Return: If the function fails - NULL.
  * Otherwise - the address of the new node.
